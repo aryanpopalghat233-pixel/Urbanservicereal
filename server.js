02d7598e9ccc015d1c5c90c46399a0aae458b0b4
aryanpopalghat233-pixel <aryanpopalghat233@gmail.com>
2026-04-29 00:57:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,3 +18,8 @@ app.use("/api/workers", require("./routes/workerRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.get('/api/bookings', async (req, res) => {
+  const bookings = await Booking.find().populate('assignedWorker');
+  res.json(bookings);
+});
