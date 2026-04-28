caf9d9ed8f12a12482ebd42cbc255d0961f93002
aryanpopalghat233-pixel <aryanpopalghat233@gmail.com>
2026-04-29 00:56:18 +0530

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,11 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const bookingSchema = new mongoose.Schema({
+const BookingSchema = new mongoose.Schema({
   name: String,
-  phone: String,
-  address: String,
   service: String,
-  date: String
+  address: String,
+
+  status: {
+    type: String,
+    default: "Pending" // Pending | Approved | Rejected
+  },
+
+  assignedWorker: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Worker",
+    default: null
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model("Booking", bookingSchema);
+module.exports = mongoose.model('Booking', BookingSchema);
